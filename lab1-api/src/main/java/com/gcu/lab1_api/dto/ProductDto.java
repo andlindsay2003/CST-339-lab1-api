95a437719c77da97ccf61b321e5782b60aa43f34
andlindsay2003 <andlindsay2003@gmail.com>
2026-09-13 19:46:38 -0700

    Branch 8: Create Product Repository

---
+++ lab1-api/src/main/java/com/gcu/lab1_api/dto/ProductDto.java
@@ -0,0 +1,6 @@
+package com.gcu.lab1_api.dto;
+
+public record ProductDto(Long id, String name, String description, Double price, Integer quantity) 
+{
+
+}
--- lab1-api/src/main/java/com/gcu/lab1_api/service/ProductService.java
+++ lab1-api/src/main/java/com/gcu/lab1_api/service/ProductService.java
@@ -1,5 +1,24 @@
 package com.gcu.lab1_api.service;
 
+import java.util.List;
+
+import com.gcu.lab1_api.dto.ProductDto;
+import com.gcu.lab1_api.repository.ProductRepository;
+
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<ProductDto>getAllProducts() {
+        // Implementation for retrieving all products
+        return productRepository.findAll().stream()
+                .map(product -> new ProductDto(product.getId(), product.getName(), product.getDescription(), product.getPrice(), product.getQuantity()))
+                .toList();
+    }
+
 
 }
